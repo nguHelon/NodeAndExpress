f193c16902e9b5e89b6ad211d04e9dca8aa116ff
nguHelon <nguhelon@gmail.com>
2023-09-22 10:37:48 +0100

    learning node js and express js

---
+++ 02-express-tutorial/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+const peopleRouter = require('./routes/people')
+const loginRouter = require('./routes/auth');
+
+
+//static assets
+app.use(express.static('./methods-public'))
+// parse form data
+
+app.use(express.urlencoded({ extended: false }))
+
+// parse json
+app.use(express.json())
+
+app.use('/api/people', peopleRouter)
+
+app.use('/login', loginRouter)
+
+
+app.listen('5000', () => {
+    console.log("server is listening on port 5000...")
+})
